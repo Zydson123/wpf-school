77fc741590213e4604115c4dfc901b3ecc7c94ba
Zydson <101566833+Zydson123@users.noreply.github.com>
2022-12-15 08:23:47 +0100

    Add files via upload

---
+++ test-15_12/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test15_12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// napisz program który wyświetli listę:
+    /// do zrobienia | czy zrobione | termin
+    /// wyprowadzić psa | false | zaraz
+    /// podlać kwiaty | false | jutro
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ToDo> ToDoList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            setupList();
+
+        }
+        private void setupList()
+        {
+            ToDoList = new ObservableCollection<ToDo>();
+            ToDoList.Add(new ToDo("wyprowadzić psa", false, "zaraz"));
+            ToDoList.Add(new ToDo("podlać kwiaty", false, "jutro"));
+        }
+    }
+}
---
+++ test-15_12/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test15_12
+{
+    public class ToDo
+    {
+        public string WhatToDo { get; set; }
+        public bool IsDone { get; set; }
+        public string WhenToDo { get; set; }
+        public ToDo(string what, bool done, string when)
+        {
+            WhatToDo = what;
+            IsDone = done;
+            WhenToDo = when;
+        }
+    }
+
+}
